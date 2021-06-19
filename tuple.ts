cc0580c1cf9e079897d696faf0be161bf5b89dbd
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-20 00:29:48 +0530

    tuples in ts

--- tuple.ts
+++ tuple.ts
@@ -1,1 +1,14 @@
 // Tuple: Array like structure where each element represents some property of a record
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+const pepsi: [string, boolean, number] = ['brown', true, 20];
+
+//type alias
+type Drink = [string, boolean, number];
+
+const sprite: Drink = ['clear', true, 35];
+const tea: Drink = ['brown', false, 10];
